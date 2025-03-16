fa4bc0bbde778c4cdd4d45b693796839208b7080
william_stere <william_stere@outlook.com>
2025-03-16 11:17:04 +0800

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import json
+import requests
+from uuid import uuid4
+
+class DifyMain:
+    def __init__(self):
+        self.config = {
+            "API_BASE" : "http://ws-server/v1",
+            "WORKFLOW_ID": "workflows/run",
+            "API_KEY": "app-ridikcsO76jrR0M6DGqLdIqN",
+            "USER_INFO": "ws"
+        }
+
+        self.session = {
+            "session_id" : str(uuid4()),
+            "user_agent" : "api"
+        }
+
+    def _build_headers(self):
+        return {
+            "Authorization": f"Bearer {self.config['API_KEY']}",
+            "Content-Type": "application/json"
+        }
+    
+    def send_request(self, user_input):
+        payload = {
+            "user": self.config["USER_INFO"],
+            "inputs": {"input": user_input},
+            "session": self.session,
+            "response_mode": "blocking"
+        }
+
+        url = f"{self.config['API_BASE']}/{self.config['WORKFLOW_ID']}"
+  
+        print(f"{url}\n\n", 
+              f"{self._build_headers()}\n\n", 
+              f"{payload}\n\n")
+
+        response = requests.post(
+            url, 
+            json=payload,
+            headers=self._build_headers(),
+            timeout=360
+        )
+
+        print(f"{response.status_code}")
+
+        json_response = response.json()
+
+        print(json.dumps(json_response, indent=2, ensure_ascii=False))
+
+if __name__ == "__main__":
+    send = DifyMain()
+    send.send_request("hello")
+    print("\nEXIT")
+    
